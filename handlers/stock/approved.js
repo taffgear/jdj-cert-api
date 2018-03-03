3f45df1071ff8db23286c576a83377bb481357c6
markhorsman <mark.horsman@tam-one.nl>
2018-03-03 09:54:09 +0100

    Added some stock item fields in select queries

--- handlers/stock/approved.js
+++ handlers/stock/approved.js
@@ -2,7 +2,7 @@ const errors    = require('../../lib/classes/errors');
 
 module.exports = function (req, res, next) {
 		return req.mssql.request()
-        .query('SELECT TOP 200 dbo.Stock.ITEMNO, dbo.Stock.PGROUP, dbo.Stock.GRPCODE, dbo.Stock.LASTSER#1, dbo.Stock.PERIOD#1, dbo.Stock.CURRDEPOT, dbo.Stock.DESC#1, dbo.Stock.DESC#2, dbo.Stock.DESC#3 FROM dbo.Stock WHERE DATEADD(DAY, [PERIOD#1], [LASTSER#1]) >= GETDATE() ORDER BY LASTSER#1 DESC')
+        .query('SELECT TOP 200 dbo.Stock.RECID, dbo.Stock.ITEMNO, dbo.Stock.PGROUP, dbo.Stock.GRPCODE, dbo.Stock.LASTSER#1, dbo.Stock.PERIOD#1, dbo.Stock.CURRDEPOT, dbo.Stock.DESC#1, dbo.Stock.DESC#2, dbo.Stock.DESC#3 FROM dbo.Stock WHERE DATEADD(DAY, [PERIOD#1], [LASTSER#1]) >= GETDATE() ORDER BY LASTSER#1 DESC')
         .then(result => {
             return res.status(200).send({ success: true, body: result.recordset });
         })
--- handlers/stock/unapproved.js
+++ handlers/stock/unapproved.js
@@ -4,7 +4,7 @@ const errors    = require('../../lib/classes/errors');
 
 module.exports = function (req, res, next) {
 		return req.mssql.request()
-        .query("SELECT TOP 200 dbo.Stock.ITEMNO, dbo.Stock.PGROUP, dbo.Stock.GRPCODE, dbo.Stock.LASTSER#1, dbo.Stock.PERIOD#1, dbo.Stock.CURRDEPOT, dbo.Stock.DESC#1, dbo.Stock.DESC#2, dbo.Stock.DESC#3 FROM dbo.Stock WHERE dbo.Stock.LASTSER#1 LIKE '%1899%' ORDER BY DESC#1 ASC")
+        .query("SELECT TOP 200 dbo.Stock.RECID, dbo.Stock.ITEMNO, dbo.Stock.PGROUP, dbo.Stock.GRPCODE, dbo.Stock.LASTSER#1, dbo.Stock.PERIOD#1, dbo.Stock.CURRDEPOT, dbo.Stock.DESC#1, dbo.Stock.DESC#2, dbo.Stock.DESC#3 FROM dbo.Stock WHERE dbo.Stock.LASTSER#1 LIKE '%1899%' ORDER BY DESC#1 ASC")
         .then(result => {
             return res.status(200).send({ success: true, body: result.recordset });
         })
