374fb5464b54dd35b0d7e5f55666f96d27aef026
markhorsman <mark.horsman@tam-one.nl>
2018-04-11 17:33:00 +0200

    Settings update WIP

--- handlers/stock/approved.js
+++ handlers/stock/approved.js
@@ -1,4 +1,6 @@
 const reduce 		= require('lodash/reduce');
+const bb        = require('bluebird');
+const sql 		  = bb.promisifyAll(require("mssql"));
 const errors    = require('../../lib/classes/errors');
 
 module.exports = function (req, res, next) {
@@ -12,7 +14,8 @@ module.exports = function (req, res, next) {
 		}, []).join(',');
 
 		return req.mssql.request()
-        .query('SELECT ' + (limit && limit > 0 ? 'TOP ' + limit + ' ' : '') + 'dbo.Stock.RECID, dbo.Stock.ITEMNO, dbo.Stock.PGROUP, dbo.Stock.GRPCODE, dbo.Stock.LASTSER#1, dbo.Stock.PERIOD#1, dbo.Stock.CURRDEPOT, dbo.Stock.DESC#1, dbo.Stock.DESC#2, dbo.Stock.DESC#3 FROM dbo.Stock WHERE DATEADD(DAY, [PERIOD#1], [LASTSER#1]) >= GETDATE()' + (status.length ? ' AND dbo.Stock.STATUS IN (' + status + ')' : '') + ' ORDER BY LASTSER#1 DESC')
+				.input('status', sql.NVarChar, status)
+        .query('SELECT ' + (limit && limit > 0 ? 'TOP ' + limit + ' ' : '') + 'dbo.Stock.RECID, dbo.Stock.ITEMNO, dbo.Stock.PGROUP, dbo.Stock.GRPCODE, dbo.Stock.LASTSER#1, dbo.Stock.PERIOD#1, dbo.Stock.CURRDEPOT, dbo.Stock.DESC#1, dbo.Stock.DESC#2, dbo.Stock.DESC#3 FROM dbo.Stock WHERE DATEADD(DAY, [PERIOD#1], [LASTSER#1]) >= GETDATE() AND dbo.Stock.STATUS IN(@status) ORDER BY LASTSER#1 DESC')
         .then(result => {
             return res.status(200).send({ success: true, body: result.recordset });
         })
--- handlers/stock/unapproved.js
+++ handlers/stock/unapproved.js
@@ -14,7 +14,8 @@ module.exports = function (req, res, next) {
 	}, []).join(',');
 
 	return req.mssql.request()
-      .query("SELECT " + (limit && limit > 0 ? "TOP " + limit + " " : "") + "dbo.Stock.RECID, dbo.Stock.ITEMNO, dbo.Stock.PGROUP, dbo.Stock.GRPCODE, dbo.Stock.LASTSER#1, dbo.Stock.PERIOD#1, dbo.Stock.CURRDEPOT, dbo.Stock.DESC#1, dbo.Stock.DESC#2, dbo.Stock.DESC#3 FROM dbo.Stock WHERE dbo.Stock.LASTSER#1 LIKE '%1899%' AND dbo.Stock.PATTEST = '1'" + (status.length ? " AND dbo.Stock.STATUS IN (" + status + ")" : "") + " ORDER BY DESC#1 ASC")
+			.input('status', sql.NVarChar, status)
+			.query("SELECT " + (limit && limit > 0 ? "TOP " + limit + " " : "") + "dbo.Stock.RECID, dbo.Stock.ITEMNO, dbo.Stock.PGROUP, dbo.Stock.GRPCODE, dbo.Stock.LASTSER#1, dbo.Stock.PERIOD#1, dbo.Stock.CURRDEPOT, dbo.Stock.DESC#1, dbo.Stock.DESC#2, dbo.Stock.DESC#3 FROM dbo.Stock WHERE dbo.Stock.LASTSER#1 LIKE '%1899%' AND dbo.Stock.PATTEST = '1' AND dbo.Stock.STATUS IN(@status) ORDER BY DESC#1 ASC")
       .then(result => {
           return res.status(200).send({ success: true, body: result.recordset });
       })
